cd730c7cc0ff078321e8787f822958867a1418a0
Jon Strabala <54073900+jon-strabala@users.noreply.github.com>
2024-03-07 14:59:09 -0800

    Must be a POST

--- modules/vector-search/examples/run-vector-search-payload.sh
+++ modules/vector-search/examples/run-vector-search-payload.sh
@@ -1,4 +1,4 @@
-curl -k -XPUT -H "Content-Type: application/json" \
+curl -k -XPOST -H "Content-Type: application/json" \
     -u ${CB_USERNAME}:${CB_PASSWORD} \
     https://${CB_HOSTNAME}:18094/api/bucket/vector-sample/scope/color/index/color-index/query \
 -d '{
