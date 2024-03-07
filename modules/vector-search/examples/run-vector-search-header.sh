7b033b1dcba172c0033d0f0984f22d576288e144
Jon Strabala <54073900+jon-strabala@users.noreply.github.com>
2024-03-07 14:59:57 -0800

    Must be a POST

--- modules/vector-search/examples/run-vector-search-header.sh
+++ modules/vector-search/examples/run-vector-search-header.sh
@@ -1,4 +1,4 @@
-curl -k -XPUT -H "Content-Type: application/json" \
+curl -k -XPOST -H "Content-Type: application/json" \
     -u ${CB_USERNAME}:${CB_PASSWORD} \
     https://${CB_HOSTNAME}:18094/api/bucket/$BUCKET_NAME/scope/$SCOPE_NAME/index/$INDEX_NAME/query \
     -d '{ ... JSON query payload goes here ... }'
