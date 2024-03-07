cbc4f59deb86a7be89888ac6222ba9c16a7ff0ea
Jon Strabala <54073900+jon-strabala@users.noreply.github.com>
2024-03-07 15:53:16 -0800

    make work with colorvect_l2

--- modules/vector-search/examples/run-vector-search-payload.sh
+++ modules/vector-search/examples/run-vector-search-payload.sh
@@ -2,19 +2,19 @@ curl -k -XPOST -H "Content-Type: application/json" \
     -u ${CB_USERNAME}:${CB_PASSWORD} \
     https://${CB_HOSTNAME}:18094/api/bucket/vector-sample/scope/color/index/color-index/query \
 -d '{
-      "fields": ["*"], 
-      "query": { 
-        "min": 70, 
-        "max": 80,  
-        "inclusive_min": false,  
-        "inclusive_max": true,  
-        "field": "brightness" 
-      }, 
-      "knn": [
-        {
-          "k": 10, 
-          "field": "colorvect_dot", 
-          "vector": [ 0.707106781186548, 0, 0.707106781186548 ]
-        }
-      ]
-    }'
+  "fields": ["*"],
+  "query": {
+    "min": 70,
+    "max": 80,
+    "inclusive_min": false,
+    "inclusive_max": true,
+    "field": "brightness"
+  },
+  "knn": [
+    {
+      "k": 10,
+      "field": "colorvect_l2",
+      "vector": [ 176,  0, 176 ]
+    }
+  ]
+}'
