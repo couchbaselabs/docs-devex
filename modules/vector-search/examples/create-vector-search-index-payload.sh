e036798138bb87fa1b6b663f33e9b3e861569fb3
Jon Strabala <54073900+jon-strabala@users.noreply.github.com>
2024-03-07 12:51:22 -0800

    Make it work with Capella and no longer silent

--- modules/vector-search/examples/create-vector-search-index-payload.sh
+++ modules/vector-search/examples/create-vector-search-index-payload.sh
@@ -1,5 +1,6 @@
-curl -k -s -XPUT -H "Content-Type: application/json" \
-    -u ${CB_USERNAME}:${CB_PASSWORD} https://${CB_HOSTNAME}:18094/api/bucket/vector-sample/scope/color/index/color-index 
+curl -k -XPUT -H "Content-Type: application/json" \
+    -u ${CB_USERNAME}:${CB_PASSWORD} \
+    https://${CB_HOSTNAME}:18094/api/bucket/vector-sample/scope/color/index/color-index \
     -d \
   '{
     "type": "fulltext-index",
@@ -72,4 +73,4 @@ curl -k -s -XPUT -H "Content-Type: application/json" \
       }
     },
     "sourceParams": {}
-  }
+  }'
