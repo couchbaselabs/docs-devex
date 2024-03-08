9fd07ca6ca5d7fa02032c12763e6626c63db53aa
Jon Strabala <54073900+jon-strabala@users.noreply.github.com>
2024-03-08 13:55:42 -0800

    Update run-vector-search-header.sh

--- modules/vector-search/examples/run-vector-search-header.sh
+++ modules/vector-search/examples/run-vector-search-header.sh
@@ -1,4 +1,4 @@
 curl -k -XPOST -H "Content-Type: application/json" \
     -u ${CB_USERNAME}:${CB_PASSWORD} \
-    https://${CB_HOSTNAME}:18094/api/bucket/${BUCKET_NAME}/scope/${SCOPE_NAME}/index/${INDEX_NAME}/query \
+    https://${CB_FTSHOSTNAME}:18094/api/bucket/${BUCKET_NAME}/scope/${SCOPE_NAME}/index/${INDEX_NAME}/query \
     -d '{ ... JSON query payload goes here ... }'
