753d98ce766324470ca43baa8f6320f97b62d881
Jon Strabala <54073900+jon-strabala@users.noreply.github.com>
2024-03-08 13:55:28 -0800

    Update create-vector-search-index-payload.sh

--- modules/vector-search/examples/create-vector-search-index-payload.sh
+++ modules/vector-search/examples/create-vector-search-index-payload.sh
@@ -1,6 +1,6 @@
 curl -k -XPUT -H "Content-Type: application/json" \
     -u ${CB_USERNAME}:${CB_PASSWORD} \
-    https://${CB_HOSTNAME}:18094/api/bucket/vector-sample/scope/color/index/color-index \
+    https://${CB_FTSHOSTNAME}:18094/api/bucket/vector-sample/scope/color/index/color-index \
     -d \
   '{
   "type": "fulltext-index",
