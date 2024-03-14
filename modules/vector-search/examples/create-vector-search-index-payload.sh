abdde93870b2a959c65ac862252b680a67c732d6
Sarah Welton <sarah.welton@couchbase.com>
2024-03-14 10:45:15 -0400

    [AV-68571] Adding attributes to handle product and UI names to future-proof updates.
    Adding proper expansion formatting to variables in REST API paths/curl commands

--- modules/vector-search/examples/create-vector-search-index-payload.sh
+++ modules/vector-search/examples/create-vector-search-index-payload.sh
@@ -1,5 +1,5 @@
 curl -s -XPUT -H "Content-Type: application/json" \
-    -u $CB_USERNAME:$CB_PASSWORD https://$CB_HOSTNAME:18094/api/bucket/vector-sample/scope/color/index/color-index 
+    -u ${CB_USERNAME}:${CB_PASSWORD} https://${CB_HOSTNAME}:18094/api/bucket/vector-sample/scope/color/index/color-index 
     -d \
   '{
     "type": "fulltext-index",
--- modules/vector-search/examples/run-vector-search-header.sh
+++ modules/vector-search/examples/run-vector-search-header.sh
@@ -1,2 +1,2 @@
 curl -s -XPUT -H "Content-Type: application/json" \
--u $CB_USERNAME:$CB_PASSWORD https://$CB_HOSTNAME:18094/api/bucket/$BUCKET_NAME/scope/$SCOPE_NAME/index/$INDEX_NAME/query -d \
+-u ${CB_USERNAME}:${CB_PASSWORD} https://${CB_HOSTNAME}:18094/api/bucket/${BUCKET_NAM}E/scope/${SCOPE_NAME}/index/${INDEX_NAME}/query -d \
