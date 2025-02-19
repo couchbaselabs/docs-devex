6f423c85e3481624a689ef9d3a8997ba922ac10c
Rakhi Prathap <rakhi.prathap@couchbase.com>
2025-02-19 10:15:53 +0530

    add new page for inserting records

--- modules/tutorials/examples/ConnectStudent.java
+++ modules/tutorials/examples/ConnectStudent.java
@@ -10,14 +10,14 @@ public class ConnectStudent {
     public static void main(String[] args) {
 
         String endpoint = "<<connection-string>>"; // Replace this with Connection String
-		String username = "<<username>>"; // Replace this with username from cluster access credentials
-		String password = "<<password>>"; // Replace this with password from cluster access credentials
-       
-		// Connecting to the cluster
-		Cluster cluster = Cluster.connect(endpoint, ClusterOptions.clusterOptions(username, password)
-		// Use the pre-configured profile below to avoid latency issues with your connection.
-						.environment(env -> env.applyProfile("wan-development"))
-		);
+        String username = "<<username>>"; // Replace this with username from cluster access credentials
+        String password = "<<password>>"; // Replace this with password from cluster access credentials
+        
+        //Connecting to the cluster
+        Cluster cluster = Cluster.connect(endpoint, ClusterOptions.clusterOptions(username, password)
+        // Use the pre-configured profile below to avoid latency issues with your connection.
+                    .environment(env -> env.applyProfile("wan-development"))
+        );
 
         // The `cluster.bucket` retrieves the bucket you set up for the student cluster.
         Bucket bucket = cluster.bucket("student-bucket");  
