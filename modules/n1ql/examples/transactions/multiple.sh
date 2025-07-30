5656623f934c49d3188b5ff29f2ba6e7a73b21cf
Simon Dew <Simon.Dew@couchbase.com>
2025-07-30 16:59:26 +0100

    Update examples

--- modules/n1ql/examples/transactions/multiple.sh
+++ modules/n1ql/examples/transactions/multiple.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # tag::index[]
-curl http://localhost:8093/query/service \
--u Administrator:password \
--H 'Content-Type: application/json' \
--d '{
+curl -X POST "$BASEURL/_p/query/query/service" \
+  -u $USER:$PASSWORD \
+  -H 'Content-Type: application/json' \
+  -d '{
   "statement": "CREATE PRIMARY INDEX ON bookings;",
   "query_context": "`travel-sample`.tenant_agent_00",
 }'
@@ -12,10 +12,10 @@ curl http://localhost:8093/query/service \
 
 # tag::transaction[]
 # tag::begin[]
-curl http://localhost:8093/query/service \
--u Administrator:password \
--H 'Content-Type: application/json' \
--d '{
+curl -X POST "$BASEURL/_p/query/query/service" \
+  -u $USER:$PASSWORD \
+  -H 'Content-Type: application/json' \
+  -d '{
   "statement": "BEGIN WORK",
   "query_context": "`travel-sample`.tenant_agent_00",
   "txtimeout": "2m",
@@ -25,10 +25,10 @@ curl http://localhost:8093/query/service \
 # end::begin[]
 
 # tag::set[]
-curl http://localhost:8093/query/service \
--u Administrator:password \
--H 'Content-Type: application/json' \
--d '{
+curl -X POST "$BASEURL/_p/query/query/service" \
+  -u $USER:$PASSWORD \
+  -H 'Content-Type: application/json' \
+  -d '{
   "statement": "SET TRANSACTION ISOLATION LEVEL READ COMMITTED;",
   "query_context": "`travel-sample`.tenant_agent_00",
   "txid": "d81d9b4a-b758-4f98-b007-87ba262d3a51"
@@ -36,10 +36,10 @@ curl http://localhost:8093/query/service \
 # end::set[]
 
 # tag::upsert[]
-curl http://localhost:8093/query/service \
--u Administrator:password \
--H 'Content-Type: application/json' \
--d '{
+curl -X POST "$BASEURL/_p/query/query/service" \
+  -u $USER:$PASSWORD \
+  -H 'Content-Type: application/json' \
+  -d '{
   "statement": "UPSERT INTO bookings VALUES(\"bf7ad6fa-bdb9-4099-a840-196e47179f03\", {
     \"date\": \"07/24/2021\",
     \"flight\": \"WN533\",
@@ -53,10 +53,10 @@ curl http://localhost:8093/query/service \
 # end::upsert[]
 
 # tag::savepoint-1[]
-curl http://localhost:8093/query/service \
--u Administrator:password \
--H 'Content-Type: application/json' \
--d '{
+curl -X POST "$BASEURL/_p/query/query/service" \
+  -u $USER:$PASSWORD \
+  -H 'Content-Type: application/json' \
+  -d '{
   "statement": "SAVEPOINT s1;",
   "query_context": "`travel-sample`.tenant_agent_00",
   "txid": "d81d9b4a-b758-4f98-b007-87ba262d3a51"
@@ -64,10 +64,10 @@ curl http://localhost:8093/query/service \
 # end::savepoint-1[]
 
 # tag::update-1[]
-curl http://localhost:8093/query/service \
--u Administrator:password \
--H 'Content-Type: application/json' \
--d '{
+curl -X POST "$BASEURL/_p/query/query/service" \
+  -u $USER:$PASSWORD \
+  -H 'Content-Type: application/json' \
+  -d '{
   "statement": "UPDATE bookings AS b
     SET b.`user` = \"0\"
     WHERE META(b).id = \"bf7ad6fa-bdb9-4099-a840-196e47179f03\";",
@@ -77,10 +77,10 @@ curl http://localhost:8093/query/service \
 # end::update-1[]
 
 # tag::check-1[]
-curl http://localhost:8093/query/service \
--u Administrator:password \
--H 'Content-Type: application/json' \
--d '{
+curl -X POST "$BASEURL/_p/query/query/service" \
+  -u $USER:$PASSWORD \
+  -H 'Content-Type: application/json' \
+  -d '{
   "statement": "SELECT b.*, u.name
                 FROM bookings b JOIN users u ON b.`user` = META(u).id
                 WHERE META(b).id = \"bf7ad6fa-bdb9-4099-a840-196e47179f03\";",
@@ -90,10 +90,10 @@ curl http://localhost:8093/query/service \
 # end::check-1[]
 
 # tag::savepoint-2[]
-curl http://localhost:8093/query/service \
--u Administrator:password \
--H 'Content-Type: application/json' \
--d '{
+curl -X POST "$BASEURL/_p/query/query/service" \
+  -u $USER:$PASSWORD \
+  -H 'Content-Type: application/json' \
+  -d '{
   "statement": "SAVEPOINT s2;",
   "query_context": "`travel-sample`.tenant_agent_00",
   "txid": "d81d9b4a-b758-4f98-b007-87ba262d3a51"
@@ -101,10 +101,10 @@ curl http://localhost:8093/query/service \
 # end::savepoint-2[]
 
 # tag::update-2[]
-curl http://localhost:8093/query/service \
--u Administrator:password \
--H 'Content-Type: application/json' \
--d '{
+curl -X POST "$BASEURL/_p/query/query/service" \
+  -u $USER:$PASSWORD \
+  -H 'Content-Type: application/json' \
+  -d '{
   "statement": "UPDATE bookings AS b
     SET b.`user` = \"1\"
     WHERE META(b).id = \"bf7ad6fa-bdb9-4099-a840-196e47179f03\";",
@@ -114,10 +114,10 @@ curl http://localhost:8093/query/service \
 # end::update-2[]
 
 # tag::check-2[]
-curl http://localhost:8093/query/service \
--u Administrator:password \
--H 'Content-Type: application/json' \
--d '{
+curl -X POST "$BASEURL/_p/query/query/service" \
+  -u $USER:$PASSWORD \
+  -H 'Content-Type: application/json' \
+  -d '{
   "statement": "SELECT b.*, u.name
                 FROM bookings b JOIN users u ON b.`user` = META(u).id
                 WHERE META(b).id = \"bf7ad6fa-bdb9-4099-a840-196e47179f03\";",
@@ -127,10 +127,10 @@ curl http://localhost:8093/query/service \
 # end::check-2[]
 
 # tag::rollback[]
-curl http://localhost:8093/query/service \
--u Administrator:password \
--H 'Content-Type: application/json' \
--d '{
+curl -X POST "$BASEURL/_p/query/query/service" \
+  -u $USER:$PASSWORD \
+  -H 'Content-Type: application/json' \
+  -d '{
   "statement": "ROLLBACK TRAN TO SAVEPOINT s2;",
   "query_context": "`travel-sample`.tenant_agent_00",
   "txid": "d81d9b4a-b758-4f98-b007-87ba262d3a51"
@@ -138,10 +138,10 @@ curl http://localhost:8093/query/service \
 # end::rollback[]
 
 # tag::check-3[]
-curl http://localhost:8093/query/service \
--u Administrator:password \
--H 'Content-Type: application/json' \
--d '{
+curl -X POST "$BASEURL/_p/query/query/service" \
+  -u $USER:$PASSWORD \
+  -H 'Content-Type: application/json' \
+  -d '{
   "statement": "SELECT b.*, u.name
                 FROM bookings b JOIN users u ON b.`user` = META(u).id
                 WHERE META(b).id = \"bf7ad6fa-bdb9-4099-a840-196e47179f03\";",
@@ -151,10 +151,10 @@ curl http://localhost:8093/query/service \
 # end::check-3[]
 
 # tag::commit[]
-curl http://localhost:8093/query/service \
--u Administrator:password \
--H 'Content-Type: application/json' \
--d '{
+curl -X POST "$BASEURL/_p/query/query/service" \
+  -u $USER:$PASSWORD \
+  -H 'Content-Type: application/json' \
+  -d '{
   "statement": "COMMIT TRANSACTION",
   "query_context": "`travel-sample`.tenant_agent_00",
   "txid": "d81d9b4a-b758-4f98-b007-87ba262d3a51"
