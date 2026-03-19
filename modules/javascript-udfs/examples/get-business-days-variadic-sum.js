db7d1106094e0c1bfd9f1ab1ab81e01d30e41f13
Simon Dew <Simon.Dew@couchbase.com>
2026-03-19 22:39:54 +0000

    Add update library examples

---
+++ modules/javascript-udfs/examples/get-business-days-variadic-sum.js
@@ -0,0 +1,17 @@
+function getBusinessDays(startDate, endDate) {
+  let count = 0;
+  const curDate = new Date(new Date(startDate).getTime());
+  while (curDate <= new Date(endDate)) {
+      const dayOfWeek = curDate.getDay();
+      if(dayOfWeek !== 0 && dayOfWeek !== 6)
+          count++;
+      curDate.setDate(curDate.getDate() + 1);
+  }
+  return count;
+}
+
+function sumListOfNumbers(... args) {
+  var sum = 0;
+  args.forEach(value => sum = sum  + value);
+  return sum;
+}
